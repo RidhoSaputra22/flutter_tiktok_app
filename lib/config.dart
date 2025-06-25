fc2ac161d2c8f6014d2d7cf2b812a80247601d26
ridho-code <60309031+ridho-code@users.noreply.github.com>
2025-06-26 06:13:32 +0800

    -

--- lib/config.dart
+++ lib/config.dart
@@ -1,4 +1,5 @@
 class Config {
-  static String apiBaseUrl = "http://127.0.0.1:8000";
-  // static String apiBaseUrl = "https://ridho.api.thaumazein.my.id";
+  // static String apiBaseUrl = "http://127.0.0.1:8000";
+  static String apiBaseUrl =
+      "https://klepon.my.id/apps/laravel-tiktok-api/public";
 }
